0b5f4b4e2ba6c9b5c63bd49a4df8a2e37c8d7667
Oliver <Oliver@DESKTOP-PO4QIM5>
2016-10-22 00:49:16 +0100

    Initial Deadfile commit

---
+++ Deadfile/Deadfile.Entity/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deadfile.Entity
+{
+    /// <summary>
+    /// Entity model for a Client.
+    /// </summary>
+    public sealed class Client
+    {
+
+    }
+}
