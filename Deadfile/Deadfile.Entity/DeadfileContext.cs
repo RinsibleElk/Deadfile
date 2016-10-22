209313bb3860e75bcebb43c0b214ff36d7f49a33
Oliver <Oliver@DESKTOP-PO4QIM5>
2016-10-22 01:15:43 +0100

    Strart the Entity model

---
+++ Deadfile/Deadfile.Entity/DeadfileContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deadfile.Entity
+{
+    /// <summary>
+    /// Represents a session with the Deadfile database.
+    /// </summary>
+    public class DeadfileContext : DbContext
+    {
+    }
+}
---
+++ Deadfile/Deadfile.Entity/Job.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deadfile.Entity
+{
+    public class Job
+    {
+        public int JobId { get; set; }
+
+        public string AddressFirstLine { get; set; }
+
+        public string AddressSecondLine { get; set; }
+
+        public string AddressThirdLine { get; set; }
+
+        public string AddressPostCode { get; set; }
+
+        public int ClientId { get; set; }
+
+        public virtual Client Client { get; set; }
+    }
+}
