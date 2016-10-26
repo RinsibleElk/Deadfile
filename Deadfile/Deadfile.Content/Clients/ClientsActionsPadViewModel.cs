e83ee5bfbf3210a9f55f2f83c83c30084dc95c82
Oliver <Oliver@DESKTOP-PO4QIM5>
2016-10-26 01:29:13 +0100

    Visibility changes

--- Deadfile/Deadfile.Content/Clients/ClientsActionsPadViewModel.cs
+++ Deadfile/Deadfile.Content/Clients/ClientsActionsPadViewModel.cs
@@ -27,6 +27,7 @@ namespace Deadfile.Content.Clients
         private readonly DelegateCommand _saveClientCommand;
         private readonly DelegateCommand _discardClientCommand;
         private readonly DelegateCommand _deleteClientCommand;
+        private SubscriptionToken _lockedForEditingEventSubscriptionToken = null;
 
         public ClientsActionsPadViewModel(IEventAggregator eventAggregator, IDeadfileNavigationService navigationService) : base(eventAggregator)
         {
@@ -38,37 +39,65 @@ namespace Deadfile.Content.Clients
             _deleteClientCommand = new DelegateCommand(DeleteClientAction, CanDeleteClient);
         }
 
+        public override void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            base.OnNavigatedTo(navigationContext);
+
+            // Subscribe for event from content.
+            _lockedForEditingEventSubscriptionToken =
+                EventAggregator.GetEvent<LockedForEditingEvent>().Subscribe(LockedForEditingAction);
+        }
+
+        private void LockedForEditingAction(bool lockedForEditing)
+        {
+            AddClientVisibility = lockedForEditing ? Visibility.Collapsed : Visibility.Visible;
+            SaveClientVisibility = lockedForEditing ? Visibility.Visible : Visibility.Collapsed;
+            DiscardClientVisibility = lockedForEditing ? Visibility.Visible : Visibility.Collapsed;
+            EditClientVisibility = lockedForEditing ? Visibility.Collapsed : Visibility.Visible;
+            DeleteClientVisibility = lockedForEditing ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public override void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            base.OnNavigatedFrom(navigationContext);
+
+            // Unsubscribe for event from content.
+            EventAggregator.GetEvent<LockedForEditingEvent>().Unsubscribe(_lockedForEditingEventSubscriptionToken);
+            _lockedForEditingEventSubscriptionToken = null;
+        }
+
         private void DiscardClientAction()
         {
         }
 
         private bool CanDiscardClient()
         {
-            return true;
+            //TODO
+            return _discardClientVisibility == Visibility.Visible;
         }
 
         private bool CanDeleteClient()
         {
             //TODO
-            return true;
+            return _deleteClientVisibility == Visibility.Visible;
         }
 
         private bool CanSaveClient()
         {
             //TODO
-            return false;
+            return _saveClientVisibility == Visibility.Visible;
         }
 
         private bool CanEditClient()
         {
             //TODO
-            return true;
+            return _editClientVisibility == Visibility.Visible;
         }
 
         private bool CanAddClient()
         {
             //TODO
-            return true;
+            return _addClientVisibility == Visibility.Visible;
         }
 
         public ICommand AddClientCommand { get { return _addClientCommand; } }
@@ -81,35 +110,65 @@ namespace Deadfile.Content.Clients
         public Visibility AddClientVisibility
         {
             get { return _addClientVisibility; }
-            set { SetProperty(ref _addClientVisibility, value); }
+            set
+            {
+                if (SetProperty(ref _addClientVisibility, value))
+                {
+                    _addClientCommand.RaiseCanExecuteChanged();
+                }
+            }
         }
 
         private Visibility _editClientVisibility = Visibility.Visible;
         public Visibility EditClientVisibility
         {
             get { return _editClientVisibility; }
-            set { SetProperty(ref _editClientVisibility, value); }
+            set
+            {
+                if (SetProperty(ref _editClientVisibility, value))
+                {
+                    _editClientCommand.RaiseCanExecuteChanged();
+                }
+            }
         }
 
         private Visibility _saveClientVisibility = Visibility.Collapsed;
         public Visibility SaveClientVisibility
         {
             get { return _saveClientVisibility; }
-            set { SetProperty(ref _saveClientVisibility, value); }
+            set
+            {
+                if (SetProperty(ref _saveClientVisibility, value))
+                {
+                    _saveClientCommand.RaiseCanExecuteChanged();
+                }
+            }
         }
 
         private Visibility _discardClientVisibility = Visibility.Collapsed;
         public Visibility DiscardClientVisibility
         {
             get { return _discardClientVisibility; }
-            set { SetProperty(ref _discardClientVisibility, value); }
+            set
+            {
+                if (SetProperty(ref _discardClientVisibility, value))
+                {
+                    _discardClientCommand.RaiseCanExecuteChanged();
+                }
+            }
         }
 
         private Visibility _deleteClientVisibility = Visibility.Visible;
         public Visibility DeleteClientVisibility
         {
             get { return _deleteClientVisibility; }
-            set { SetProperty(ref _deleteClientVisibility, value); }
+            set
+            {
+                if (SetProperty(ref _deleteClientVisibility, value))
+                {
+                    _deleteClientCommand.RaiseCanExecuteChanged();
+                }
+            }
         }
 
         private void EditClientAction()
