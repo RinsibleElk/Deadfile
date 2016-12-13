9d450c93e0961eb22c8b5842a59e691cd878676e
Oliver <Oliver@DESKTOP-PO4QIM5>
2016-12-13 00:38:09 +0000

    [10] Suggest titles for Clients page.

---
+++ Deadfile/Deadfile.Model/Utils/TitleUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deadfile.Model.Utils
+{
+    /// <summary>
+    /// Utilities for handling titles.
+    /// </summary>
+    public static class TitleUtils
+    {
+        /// <summary>
+        /// All the suggested titles that should show up in combo boxes.
+        /// </summary>
+        public static readonly string[] AllTitles =
+            new string[]
+            {
+                "Mr",
+                "Mrs",
+                "Ms",
+                "Mr & Mrs",
+                "Dr",
+                "Dr & Mrs",
+                "Mr & Dr",
+                "Dr & Dr",
+                "Attack Helicopter"
+            };
+    }
+}
